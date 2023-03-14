278a52fa0828fbd8640fe3e3a718b4902e71048a
RobertoNesta <robertonesta1993@gmail.com>
2023-03-14 16:53:53 +0100

    closing the for-loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,30 @@ Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specif
 Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.*/
 
 const imgEl = document.getElementById("carousel");
-const previousImg = document.getElementById("prev")
-const nextImg = document.getElementById("next")
+console.log(imgEl)
+
+const previous = document.getElementById("prev")
+const next = document.getElementById("next")
 
 const carousel = [
-    "./assets/img/01.webp",
-    "./assets/img/02.webp",
-    "./assets/img/03.webp",
-    "./assets/img/04.webp",
-    "./assets/img/05.webp"
+    '"./assets/img/01.webp"',
+    '"./assets/img/02.webp"',
+    '"./assets/img/03.webp"',
+    '"./assets/img/04.webp"',
+    '"./assets/img/05.webp"'
 ]
+
+//select the active image
+
+let activeImage = 0;
+
+//for loop
+
+for (i = 0; i < carousel.length; i++) {
+    const imgSrc = carousel[i];
+    const imgElement = `<img class="img-fluid ${i === activeImage ? 'active' : ''}" src="${imgSrc}" alt="">`
+    console.log(imgElement);
+
+   // InnerHTML
+   imgEl.innerHTML += imgElement;
+}
