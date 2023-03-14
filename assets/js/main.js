9524019682fee996d125f93a5c7c513ada5cb6b1
RobertoNesta <robertonesta1993@gmail.com>
2023-03-14 16:12:41 +0100

    creating consts and matching them to the html ids

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
 /*rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
 Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
 Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.*/
+
+const imgEl = document.getElementById("carousel");
+const previousImg = document.getElementById("prev")
+const nextImg = document.getElementById("next")
